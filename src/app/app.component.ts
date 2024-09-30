29d5d4ecd9464185ae7379c16684a7238aa9fc96
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-10-01 01:56:24 +0400

    Some boiler template code and sawl implementation for a final project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SweetAlert2Module],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'train-tickets-shop';
+
+  btnClicked() {
+    Swal.fire({
+      toast: true,
+      title: "Yello",
+      position: 'top-right',
+      timer: 15000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      }
+    })
+  }
 }
