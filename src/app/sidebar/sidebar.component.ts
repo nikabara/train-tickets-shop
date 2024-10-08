611272badb0b63e37cee876c1ea941e17f136168
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-10-08 22:09:01 +0400

    "Updated sidebar component HTML, SASS, and TS files to add toggling functionality and modify layout and styling." error with mid responsive sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-sidebar',
-  standalone: true,
-  imports: [RouterModule, RouterOutlet],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.sass'
+    selector: 'app-sidebar',
+    standalone: true,
+    imports: [RouterModule, RouterOutlet],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.sass'
 })
 export class SidebarComponent {
- 
+    sideBarToggled: boolean = false;
+
+    toggleSideBar(): void {
+        this.sideBarToggled = !this.sideBarToggled;
+    }
 }
