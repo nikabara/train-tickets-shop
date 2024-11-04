f29f2b51de502563ff6b38bff2390a61e50446a7
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-11-04 22:57:15 +0400

    "Updated home component HTML, SASS, and TS files to add sign in and sign out functionality with sweetalert2 and authentication service"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,21 +2,25 @@ import { RegisterTicket } from './../Interfaces/RegisterTicket.interface';
 import { People } from '../Interfaces/People.interface';
 import { SaveDataService } from '../services/save-data.service';
 import { SwaggerAPIService } from '../services/swagger-api.service';
-import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule\
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { AuthService } from '../services/auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [HttpClientModule],
+	imports: [HttpClientModule, CommonModule],
 	providers: [SwaggerAPIService, SaveDataService],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.sass'
 })
-export class HomeComponent {
-	constructor(private saveDataService: SaveDataService) {}
+export class HomeComponent implements OnInit {
+	constructor(private saveDataService: SaveDataService, public auth: AuthService) { }
 
-	peopleSeatData: People[] = [{ 
+	peopleSeatData: People[] = [{
 		seatId: '3afd907f-4e98-48e6-b1ec-17a8f99be306',
 		name: 'Nick',
 		surname: 'Bara',
@@ -33,11 +37,168 @@ export class HomeComponent {
 		people: this.peopleSeatData
 	}
 
-	btnClick() : void {
+	btnClick(): void {
 		// this.saveDataService.getTrainsByFromTo('თბილისი', 'ბათუმი');
 		// this.saveDataService.getVagonsByTrainId(15);
 		// this.saveDataService.getVagon(15, 'II კლასი');
 		// this.saveDataService.postTicket(this.ticketData);
 		this.saveDataService.confirmTicket("8fc2a0f3-e420-4db1-b8de-03c7b078a820");
 	}
+
+	// testing email/password --> email: "stepproject@gmail.com", password: "Stepproject123"
+	getAccessKey(_email: string, _password: string): Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.auth.signIn({ email: _email, password: _password }).subscribe(
+				(response) => {
+					localStorage.setItem('accessToken', JSON.stringify(response));
+					resolve(); // Resolve the promise if the API call is successful
+				},
+				(error) => {
+					console.error(`Error while fetching access token: ${error}`);
+					reject(error); // Reject the promise if there is an error
+				}
+			);
+		});
+	}
+
+	async swalSignInWindow(): Promise<string[]> {
+		const { value: formValues } = await Swal.fire({
+			title: "Sign In",
+			html: `
+			  <input id="swal-input1" class="swal2-input" placeholder="Email" type="email">
+			  <input id="swal-input2" class="swal2-input" placeholder="Password" type="password">
+			`,
+			showConfirmButton: true,
+			confirmButtonText: "Sign in",
+			focusConfirm: false,
+			preConfirm: () => {
+				const emailInput = document.getElementById("swal-input1") as HTMLInputElement;
+				const passwordInput = document.getElementById("swal-input2") as HTMLInputElement;
+
+				// Validate inputs explicitly
+				if (!emailInput.value || !passwordInput.value) {
+					Swal.showValidationMessage('Please enter both email and password');
+					return;
+				}
+
+				// Regular expression for basic email format validation
+				const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+				if (!emailPattern.test(emailInput.value)) {
+					Swal.showValidationMessage('Please enter a valid email address');
+					return;
+				}
+
+				return [emailInput.value, passwordInput.value];
+			}
+		});
+
+		return formValues;
+	}
+
+	async swalSignIn(): Promise<void> {
+		let formValues: string[] = await this.swalSignInWindow();
+
+		// If formValues is undefined (e.g., user closes the modal), skip the rest
+		if (!formValues) return;
+
+		console.log(formValues[0], formValues[1]);
+
+		try {
+			await this.getAccessKey(formValues[0], formValues[1]);
+
+			// After getAccessKey completes, check if the user is authenticated
+			if (formValues[0] && formValues[1] && this.auth.isAuthenticated()) {
+
+				Swal.fire({
+					title: "We'll send verifiction link to your email",
+					text: "Please check your gmail",
+					icon: "success",
+					showConfirmButton: true,
+					confirmButtonText: "Close",
+					preConfirm: () => {
+						this.auth.verifyUserEmail(formValues[0]);
+
+						if (!!this.auth.verifyUserEmail(formValues[0])) {
+							this.saveUserData();
+						}
+					}
+				});
+
+			} else {
+				Swal.fire({
+					title: "Sign in was unsuccessful",
+					text: "Your email or password is incorrect",
+					icon: "error",
+					showConfirmButton: true,
+					confirmButtonText: "Retry",
+					showDenyButton: true,
+					denyButtonText: "Close"
+				}).then((result) => {
+					if (result.isConfirmed) {
+						this.swalSignIn();
+					}
+				});
+			}
+		} catch (error) {
+			Swal.fire({
+				title: "Sign in was unsuccessful",
+				text: "An error occurred while signing in",
+				icon: "error",
+				confirmButtonText: "Retry",
+				showDenyButton: true,
+				denyButtonText: "Close"
+			}).then((result) => {
+				if (result.isConfirmed) {
+					this.swalSignIn();
+				}
+			});
+		}
+	}
+
+	saveUserData() : void {
+		const token: string = JSON.parse(localStorage.getItem('accessToken') ?? '').access_token;
+
+		this.auth.getUser(token).subscribe(
+			(response) => {
+				localStorage.setItem('userData', JSON.stringify(response));
+			},
+			(error) => {
+				console.log(error);
+			}
+		)
+	}
+
+	ngOnInit(): void {
+		// const token: string = JSON.parse(localStorage.getItem('accessToken') ?? '').access_token;
+		// console.log(token);
+		// this.auth.getUser(token).subscribe(
+		// 	(response) => {
+		// 		console.log(response);
+		// 	},
+		// 	(error) => {
+		// 		console.log(error);
+		// 	}
+		// )
+	}
+
+	swalSignOut() : void {
+		Swal.fire({
+			html: `
+			<video class="train-go-video" autoplay loop>
+				<source class="heree" src="/train-go-animation.webm" type="video/webm">
+		  	</video>
+			` ,
+			showCancelButton: true,
+			cancelButtonText: 'Close',
+			cancelButtonColor: "#3581c6",
+			showConfirmButton: true,
+			confirmButtonText: 'Sign Out',
+			confirmButtonColor: '#ea2929',
+			imageHeight: "300px"
+		}).then((result) => {
+			if (result.isConfirmed) {
+				this.auth.signOut();
+			}
+		})
+	}	
 }
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@angular/core';
+import { Observable, tap, map } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiURL = 'https://api.everrest.educata.dev/auth'
+
+  constructor(private http: HttpClient) { }
+
+  isAuthenticated() : boolean {
+    return !!localStorage.getItem("accessToken");
+  }
+
+  signOut() : void {
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('userData');
+    window.location.reload();
+  }
+
+  signIn(userData: any) : Observable<any> {
+    return this.http.post<any>(`${this.apiURL}/sign_in`, userData);
+  }
+
+  signUp(userData: any) : Observable<any> {
+    return this.http.post<any>(`${this.apiURL}/sign_up`, userData);
+  }
+
+  signInTap(email: string, password: string): Observable<boolean> {
+    return this.http.post<{token: string}>(`${this.apiURL}/sign_in`, { email, password }).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.token);
+      }),
+      map(() => true)
+    );
+  }
+
+  getUser(token: string) : Observable<any> {
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+
+    return this.http.get<any>(this.apiURL, { headers })
+  }
+
+  refreshToken(refreshToken: string) : Observable<any> {
+    return this.http.post<any>(`${this.apiURL}/refresh`, { refresh_token: refreshToken });
+  }
+
+  updateAccessToken(accessToken: string) : void {
+    localStorage.setItem('accessToken', JSON.stringify(accessToken));
+  }
+
+  verifyUserEmail(email: string) : Observable<any> {
+    return this.http.post<any>(`${this.apiURL}/verify_email`, { email });
+  }
+}
+
+
+// {
+//   "firstName": "nick",
+//   "lastName": "baratashvili",
+//   "age": 17,
+//   "email": "stepproject@gmail.com",
+//   "password": "Stepproject123",
+//   "address": "Some address",
+//   "phone": "+995577894516",
+//   "zipcode": "3301",
+//   "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s",
+//   "gender": "MALE"
+// }
