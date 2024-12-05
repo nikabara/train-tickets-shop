b3f6cb0b205f34365f25172fcf83559bd52c451f
nikoloz baratashvili <n.baratashvili@itstep.ge>
2024-12-05 22:20:07 +0400

    **Added full translation support to :** 1) __home.component.ts__ 2) __Footer component__
    **Files Updated :**
    1) i18n/eng.json
    2) i18n/geo.json
    
    **Imported TranslatedModule in __footer.component.ts__**

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.sass'
 })
